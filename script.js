1d5d90a8062932af7bad2deecb2fe0ebb0e2bdf8
SergejBeschastnov <svb19.09@yandex.ru>
2023-05-19 14:31:39 +0600

    video-block added+favicon

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const videoFrame = document.querySelector('.video-frame');
-console.log(videoFrame.muted)
-const playBtn = document.querySelector('.video__controller');
+const playBtn = document.querySelector('.video-control');
+const playBtnIcon = document.querySelector('.video-control__icon');
 
 playBtn.addEventListener('click',()=>{
     if(videoFrame.getAttribute('data-play') === 'false'){
         videoFrame.setAttribute('data-play', 'true');
+        playBtnIcon.src= './img/pause.svg'
         videoFrame.play()
     } else{
         videoFrame.setAttribute('data-play', 'false');
+        playBtnIcon.src = './img/play.svg'
+        videoFrame.pause();
+    }
+})
+
+videoFrame.addEventListener('click',()=>{
+    if(videoFrame.getAttribute('data-play') === 'false'){
+        videoFrame.setAttribute('data-play', 'true');
+        playBtnIcon.src= './img/pause.svg'
+        videoFrame.play()
+    } else{
+        videoFrame.setAttribute('data-play', 'false');
+        playBtnIcon.src = './img/play.svg'
         videoFrame.pause();
     }
 })

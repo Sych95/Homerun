ed6d7061634177371d6a330b012977efc28a7d99
SergejBeschastnov <svb19.09@yandex.ru>
2023-05-27 03:08:47 +0600

    Animation added

--- script.js
+++ script.js
@@ -31,9 +31,17 @@ const burgerNav = document.querySelector('.nav-burger');
 
 burgerBtn.addEventListener('click',()=>{
     if(!burgerNav.classList.contains('active')){
-        burgerNav.classList.add('active')
+        burgerNav.classList.add('active');
+        burgerNav.classList.remove("slideOutUp");
+        burgerNav.classList.add("slideInDown");
+        burgerBtn.classList.remove('burger-off');
+        burgerBtn.classList.add('burger-on')
     } else{
-        burgerNav.classList.remove('active') 
+        burgerNav.classList.remove("slideInDown");
+        burgerNav.classList.add("slideOutUp");
+        setTimeout(()=> burgerNav.classList.remove('active'), 900)
+        burgerBtn.classList.remove('burger-on');
+        burgerBtn.classList.add('burger-off')
     }
 })
 
